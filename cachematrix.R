24bfeedeb414fa8369081302cc5395bd2ac35566
NitinMahajan1 <nitin_mahajan@icloud.com>
2015-05-23 21:37:11 +0100

    Code Done, Comments to be updated in code
    Comments yet to be added and make it ready for final submission

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 #Test PUSH1
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -13,4 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  message("getting OTHER data")
+  q<- print("getting OTHER data")
+  x$setsolve(s)
+  s
+  
+  }
